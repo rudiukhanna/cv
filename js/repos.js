5f449d5f28eafa5bd8d269722568b6f39be7c832
Rudiuk Hanna <melomanna@gmail.com>
2023-04-20 00:43:31 +0300

    h/w Lesson_34: token was changed

--- js/repos.js
+++ js/repos.js
@@ -1,46 +1,49 @@
-class GitHubAPI {
-    constructor(token, login) {
+class GithubApi {
+    constructor(token, username) {
       this.token = token;
-      this.login = login;
+      this.username = username;
     }
   
-    async getRepos() {
-      const response = await fetch(`https://api.github.com/users/${this.login}/repos`, {
-        headers: {
-          Authorization: `Bearer  ${this.token}`,
-        },
-      });
-      const repos = await response.json(); 
-      return repos;
+    getRepos() {
+      const headers = new Headers();
+      headers.append('Authorization', `Token ${this.token}`);
+  
+      return fetch(`https://api.github.com/users/${this.username}/repos`, { headers })
+        .then(response => response.json())
+   
     }
   }
   
-  const githubAPI = new GitHubAPI('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', 'rudiukhanna');
-  githubAPI.getRepos().then((repos) => {
-    const projectsSection = document.getElementById('projects-section');
-
-    repos.forEach(repo => {
-        const repoCard = document.createElement('div');
-        repoCard.classList.add('repo-card');
-        repoCard.style.border = '1px solid black';
-        repoCard.style.padding = '10px';
-        repoCard.style.marginBottom = '15px';
-
-        const repoLink = document.createElement('a');
-        repoLink.href = repo.html_url;
-        repoLink.textContent = repo.full_name;
-        repoCard.appendChild(repoLink);
-        repoLink.style.textDecoration = 'underline';
-
-        if (repo.description) {
-            const repoDescription = document.createElement('p');
+  
+  const api = new GithubApi('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', 'rudiukhanna');
+  document.addEventListener('DOMContentLoaded', function () {
+    api.getRepos()
+      .then(data => {
+        const projectsSection = document.getElementById('projects-section');
+  
+        data.forEach(repo => {
+          const repoCard = document.createElement('div');
+          repoCard.classList.add('repo-card');
+          repoCard.style.border = '1px solid black';
+          repoCard.style.padding = '10px';
+          repoCard.style.marginBottom = '15px';
+  
+          const repoLink = document.createElement('a');
+          repoLink.href = repo.html_url;
+          repoLink.textContent = repo.full_name;
+          repoCard.appendChild(repoLink);
+          repoLink.style.textDecoration = 'underline';
+  
+          if (repo.description) {
+            var repoDescription = document.createElement('p');
             repoDescription.textContent = repo.description;
             repoCard.appendChild(repoDescription);
             repoDescription.style.margin = '0';
-        }
-
-        projectsSection.appendChild(repoCard);
-    });
-});
+          }
   
+          projectsSection.appendChild(repoCard);
+        });
+      })
+    
+  });
   
